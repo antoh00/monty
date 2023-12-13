37705a8dfe1f6f606b036bfe2e5ffa6a9883e6a0
antoh00 <otienoantoh@gmail.com>
2023-12-13 23:20:06 +0300

    monty programme backbone

---
+++ monty.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+bus_t bus = {NULL, NULL, NULL, 0};
+
+int main(int argc, char *argv[])
+{
+	FILE *file;
+	if(argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	bus.file = file;
+			if (!file)
+			{
+				fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+				exit(EXIT_FAILURE);
+			}
+	return (0);
+}
